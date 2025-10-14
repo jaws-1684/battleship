cbd898f59387ad2eaee014392e652b37a80df518
dan <dis.131998@gmail.com>
2025-10-14 12:08:36 +0300

    create: the Ship class

---
+++ test/ship.test.js
@@ -0,0 +1,18 @@
+import { Ship } from "../src/ship.js"
+
+describe("Ship class", () => {
+  it("hit() method returns undefined", () => {
+    let ship = new Ship(1)
+    expect(ship.hit()).toBe(undefined)
+  })
+  it("isSunk() method that returns true if ship is sank", () => {
+    let ship = new Ship(1)
+    ship.hit()
+    expect(ship.isSunk()).toBe(true)
+  })
+  it("isSunk() method that returns false if ship is not sank", () => {
+    let ship = new Ship(4)
+    ship.hit()
+    expect(ship.isSunk()).toBe(false)
+  })
+})
