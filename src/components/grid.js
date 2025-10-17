6d203e9fcd53d4231bee84ab60ac638d6dac0e14
dan <dis.131998@gmail.com>
2025-10-17 13:45:39 +0300

    add: grid component with grid and addColTags functions

---
+++ src/components/grid.js
@@ -0,0 +1,40 @@
+export const Grid = ((doc) => {
+	const build = (array, type) => {
+		let grid = doc.createElement("div")
+		grid.setAttribute("class", `battlefield battlefield_${type}`)
+
+		for (let i = 0; i < array.length; i++) {
+			let row = doc.createElement("div")
+			row.setAttribute("class", "row")
+			for (let j = 0; j < array[i].length; j++) {
+				let content = array[i][j]
+				let col = doc.createElement("div")
+				col.setAttribute("data-x", i)
+				col.setAttribute("data-y", j)
+				addColTags(col, content)
+
+				row.append(col)
+			}
+			grid.append(row)
+		}
+		return grid
+	}
+	const addColTags = (col, data) => {
+		col.classList.add("col", "cell")
+
+		switch (data) {
+			case "hit":
+				col.classList.add("hit")
+				break
+			case "missed-hit":
+				col.classList.add("missed-hit")
+				break
+			case null:
+				col.classList.add("empty")
+				break
+			default:
+				col.classList.add("empty")
+		}
+	}
+	return { build }
+})(document)
