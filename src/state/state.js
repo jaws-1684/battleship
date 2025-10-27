5f00e11cdb07994fde15db89a972c5352ffb5632
dan <dis.131998@gmail.com>
2025-10-27 18:26:21 +0200

    Deployment commit

--- src/state/state.js
+++
@@ -1,9 +0,0 @@
-import { EventHandler } from "../event_handlers/event_handler.js"
-import InitState from "./init_state.js";
-import RunState from "./run_state.js";
-
-let State = {};
-State.Init = InitState;
-State.Run = RunState;
-
-export { State };
--- test/player.test.js
+++
@@ -1,8 +0,0 @@
-import Player from "../src/models/player.js"
-
-describe("Player class", () => {
-  it("initializes with a name", () => {
-    let player = new Player("spongebob")
-    expect(player.name).toBe("spongebob")
-  })
-})
